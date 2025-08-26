5bf15175ef9288df391c77028673fd5ef68a6a47
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-27 05:23:09 +0900

    feat: add stack project

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "haskell-mytutorial";
+  description = "nix-flakes-haskell";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
